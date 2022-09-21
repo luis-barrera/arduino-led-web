ff6068a14cc1d2f044804f32db906c915ed91876
Luis Barrera <luisvaldovinosbarrera@gmail.com>
2022-09-21 00:48:10 -0500

    Refactor
    - Adds comments.

--- src/main.cpp
+++ src/main.cpp
@@ -57,70 +57,8 @@ void loop() {
 
   // If a new client connects
   if (client) {
-
+    // Handle the client and set the headers
     handle_client(client, &header);
-    // // Make a String to hold incoming data from the client
-    // String currentLine = "";
-
-    // // Loop while the client's connected
-    // while (client.connected()) {
-
-    //   // If there's bytes to read from the client,
-    //   if (client.available()) {
-
-    //     // Read a byte
-    //     char c = client.read();
-
-    //     // Adds client's data to header variable
-    //     header += header + c;
-
-    //     // If the byte is a newline character
-    //     if (c == '\n') {
-    //     // If the current line is blank, you got two newline characters in a row
-    //     // that's the end of the client HTTP request, so send a response:
-
-    //       if (currentLine.length() == 0) {
-    //         // HTTP headers always start with a response code (e.g. HTTP/1.1 200
-    //         // OK) and a content-type so the client knows what's coming, then a
-    //         // blank line:
-    //         client.println("HTTP/1.1 200 OK");
-    //         client.println("Content-type:text/html");
-    //         client.println("Connection: close");
-    //         client.println();
-
-    //         // Reacts to header's content
-    //         if (header.indexOf("GET /on") >= 0) {
-    //           led_on();
-    //         } else if (header.indexOf("GET /off") >= 0) {
-    //           led_off();
-    //         }
-
-    //         // TODO: create function that reads header content to display only
-    //         // one button
-    //         client.println(HTML);
-
-    //         // To end the response to client, print an empty line
-    //         client.println();
-
-    //         // Break connection to client
-    //         break;
-
-    //       // Cleans contens readed from client
-    //       } else {
-    //         currentLine = "";
-    //       }
-
-    //     } else if (c != '\r'){
-    //       currentLine += c;
-    //     }
-    //   }
-    // }
-
-    // // Clear the header variable
-    // header = "";
-
-    // // Close the connection
-    // client.stop();
   }
 }
 
@@ -191,16 +129,19 @@ void handle_client(WiFiClient client, String* header_ptr){
   client.stop();
 }
 
-// TODO
-// void handle_header(String* header_ptr){
-//   String header_str = *header_ptr;
-//   // turns the GPIOs on and off
-//   if (header.indexOf("GET /on") >= 0) {
-//     led_on();
-//   } else if (header.indexOf("GET /off") >= 0) {
-//     led_off();
-//   }
-// }
+// Handle contents in header
+void handle_header(String* header_ptr){
+  // Convert ptr to string
+  String header = *header_ptr;
+
+  // Reacts to header's content
+  if (header.indexOf("GET /on") >= 0) {
+    led_on();
+  } else if (header.indexOf("GET /off") >= 0) {
+    led_off();
+  }
+}
+
 
 void led_on() {
   analogWrite(RGB_BLUE_PIN, 255);
